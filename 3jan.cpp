b1d56ef95d32cb3cd5cc6c3295ad7f181d72d4aa
dishant1123 <dishantshah1417@gmail.com>
2026-01-03 19:19:45 +0530

    ...

--- 3jan.cpp
+++ 3jan.cpp
@@ -107,3 +107,45 @@ after  delete :  {12,14,16,20}
 user index : 3 
 after  delete :  {12,14,16,20}
 */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int  a[50],num,i,index,del; 
+    cout<<"enter the size of array : ";
+    cin>>num; 
+    for(i=0; i<num; i++)
+    {
+        cin>>a[i]; 
+    }
+    cout<<"before delete array element is  : "<<endl;
+    for(i=0; i<num; i++)
+    {                       //          0  1  2  3  4  
+        cout<<a[i]<<endl;    // a[5] = {10,20,30,50}
+    }
+    cout<<"enter the  index of element you want to delete : ";
+    cin>>index; //3
+    for(i=index; i<num-1;i++)// i=3  3 < 4 
+    {
+        a[i] =a[i+1];  // a[3]  =a[4]
+    }
+    num--; 
+    cout<<  "after delete array element is  : "<<endl;
+    for(i=0; i<num; i++)
+    {
+        cout<<a[i]<<endl;    // a[5] = {10,20,30,50}
+    }
+    return 0; 
+}
+
+// task  :1 
+/*
+int a[5] = {10,45,67,23,90} 
+
+user enter the element : 90 
+print index number  of  90  : 4 
+*/
+
+// duplicate  : 
+
