0d1311e8e6115064419bba15dd72f025b183d807
dishant1123 <dishantshah1417@gmail.com>
2026-01-24 19:26:58 +0530

    ....

--- 24jan.cpp
+++ 24jan.cpp
@@ -106,3 +106,109 @@ condition  : for  withdraw ==> maintain 10000  rs balance  that  means
 
 function  : login  , deposit, withdraw , check balance
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+string saveduser, savedpass;
+int balance =25000; 
+
+bool login()
+{   
+    string username, password;
+    cout<<"enter the username : ";
+    cin>>username;
+    cout<<"enter the password : ";
+    cin>>password;
+    if(username ==saveduser && password ==savedpass)
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
+}
+void deposit()
+{
+    int amt; 
+    cout<<"enter the amount for deposit : ";
+    cin>>amt;
+    balance +=amt; 
+    cout<<"deposited amount : "<<amt<<endl;
+}
+void  withdraw()
+{
+    int  amt; 
+    cout<<"enter the amount for withdraw : ";  // 25000   ==>maintain  balance  : 10000 
+    cin>>amt; // after  balance =30000
+    
+    if(balance -amt >=10000) // 30000 -2500 >=10000  
+    {
+        balance -=amt; 
+        cout<<"withdrawed amount : "<<amt<<endl;
+    }
+    else 
+    {
+        cout<<"maintian 10000 rs balance in your account"<<endl;
+    }
+}
+void  checkbalance()
+{
+    cout<<"your balance : "<<balance<<endl;
+}
+
+int main()
+{
+    int  choice; 
+    cout<<"ROYAL KID BANK"<<endl;
+    cout<<"enter  the  user name  : "; 
+    cin>>saveduser;
+    cout<<"enter  the  password  : ";
+    cin>>savedpass;
+
+    if(!login())
+    {
+        cout<<"invalid username or password"<<endl;
+        return 0;
+    }
+    cout<<"login successful"<<endl;
+    cout<<"login is successful your  intial balance is  : "<<balance<<endl;
+
+    do{
+        cout<<"MENU"<<endl;
+        cout<<"1.deposit"<<endl;
+        cout<<"2.withdraw"<<endl;
+        cout<<"3.check balance"<<endl;
+        cout<<"4.exit"<<endl;
+        cout<<"enter your choice : ";
+        cin>>choice; 
+        switch(choice)
+        {
+            case 1: 
+                deposit();
+                break;
+            case 2:
+                withdraw();
+                break;
+            case 3:
+                checkbalance();
+                break;
+            case 4 :
+                cout<<"bye bye"<<endl;
+                break;
+            default:
+                cout<<"invalid choice"<<endl;
+                break;
+        }
+
+    }while(choice !=4);
+    return 0; 
+}
+
+/*
+task : 
+1. give a  login attempt  maximum attempt is  3 times.
+2. if  user select  withdraw  option then ask you to enter the  password and also  verify the  password if  it is  correct then  only  withdraw doen  otherwise  not. 
+
+*/
