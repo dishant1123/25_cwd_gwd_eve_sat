9e484bf92c0c4274af5f261a3d6da073fc94fc64
dishant1123 <dishantshah1417@gmail.com>
2026-02-21 19:51:45 +0530

    ...

--- 21feb.cpp
+++ 21feb.cpp
@@ -84,7 +84,7 @@ int main()
 */ 
 
 // structure with  pointer  :  
-
+/*
 #include <iostream>
 using namespace std;
 struct student
@@ -95,7 +95,7 @@ struct student
 
 int main()
 {
-    /*
+    
     student s={101,"jagrut"};
     student s1={102,"aksh"};
 
@@ -107,7 +107,7 @@ int main()
     
     cout<<"id of 2 student is  : "<<q->id<<endl;
     cout<<"name of 2 student is  : "<<q->name<<endl;
-    */ 
+     
 
     student s[2] ;
     student *p=s;
@@ -125,7 +125,7 @@ int main()
     }
     return 0; 
 }
-
+*/
 /*
 int a[50],  n ; 
 int  *p =a ; 
@@ -135,3 +135,129 @@ for(int  i=0; i<n; i++)
     cin>>(p+i); 
 }
 */
+
+/*
+oops : object  oriented  programming
+
+class  :data member , function  , blueprint of  object 
+
+object : real entity , instance  of class .
+
+ex :  
+
+fruits :   ==> class 
+        apple  kiwi  mango  ==> object 
+
+access specifier  : 
+
+1. public  : accessible  from anywhere
+2. private : within  the class only  
+3. protected: within  the class  and  derived  class  (intherited)
+
+pillar : 
+
+1. inheritance  : 5 types  inheritance 
+2. polymorphism : 2 type  ==> run  ,  complie 
+3. encapsulation : 2 method  ==> 1.get  2. set 
+4. abstraction  : abstract class , abstract method/function 
+*/
+
+// ex :1 
+/*
+#include <iostream>
+using namespace std;
+class student 
+{
+    public :   // id name  ==> class data member
+        int id=1; 
+        string  name="suhani"; 
+    void show()
+    {
+        cout<<"id of student is  : "<<id<<endl;
+        cout<<"name of student is  : "<<name<<endl;
+    }
+};
+int main()
+{
+    student s;
+    // cout<<"id of student is  : "<<s.id<<endl;
+    // cout<<"name of student is  : "<<s.name<<endl;
+    s.show();
+    s.name ="rutvi";
+    s.id =102; 
+
+    s.show(); 
+    return 0; 
+}
+*/
+
+// ex :2 
+/*
+#include <iostream>
+using namespace std;
+class student 
+{
+    public :   // id name  ==> class data member
+        int id; 
+        string  name;
+    void input()
+    {
+        cout<<"enter id of student : ";
+        cin>>id; 
+        cout<<"enter name of student : ";
+        cin>>name;
+    }    
+    void show()
+    {
+        cout<<"id of student is  : "<<id<<endl;
+        cout<<"name of student is  : "<<name<<endl;
+    }
+};
+int main()
+{
+    student s; 
+    s.input();  //  hemil 101  
+    s.show();
+
+    s.name ="jagrut"; 
+    s.id =103; 
+
+    s.show(); 
+    return 0; 
+}
+*/
+
+// ex :3 private  : 
+
+#include <iostream>
+using namespace std;
+class student 
+{
+    private : 
+        int id=1 ; 
+        string name ="aksh"; 
+    // public :
+    void show()
+    {
+        cout<<"id of student is  : "<<id<<endl;
+        cout<<"name of student is  : "<<name<<endl;
+    }
+    public : 
+    void display()
+    {
+        show();
+    }
+};
+int main()
+{
+    student s; 
+    // cout<<"id of student is  : "<<s.id<<endl;
+    // cout<<"name of student is  : "<<s.name<<endl;
+    // s.show();
+    s.display(); 
+    
+    return 0; 
+}
+ /*
+ s.show(); 
+ */
