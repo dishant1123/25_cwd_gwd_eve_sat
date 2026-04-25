bba5eb26a5e2c52dc8f1f71e1a38c05ce3343f9a
dishant1123 <dishantshah1417@gmail.com>
2026-04-25 19:35:39 +0530

    ....

--- 25april.cpp
+++ 25april.cpp
@@ -11,7 +11,7 @@ vector <int> s;
 */
 
 // ex :1 
-
+/*
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -24,17 +24,19 @@ int  main()
     s.push_back(67);
     s.push_back(89);
 
-    /*
-    for(i=0; i<5; i++)
-        cout<<a[i]; 
-    */
+    
+    //for(i=0; i<5; i++)
+    //    cout<<a[i]; 
+    
 
     for(int i=0; i<s.size(); i++)
     {
         cout<<s[i]<<" ";
     }
+
     return 0 ;
 }
+*/ 
 
 /*
 1 element  update   ==> 23   ===> 235 
@@ -42,3 +44,102 @@ remove    ==> pop_back()
             2. pop_back(2) 
 print  vector 
 */
+
+// ex :2 
+/*
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector <int>s;
+    s.push_back(230);
+    s.push_back(45);
+    s.push_back(67);
+    s.push_back(89);
+
+    cout <<"before sorting vectore  element  is  : "<<endl;
+
+    for(int i=0; i<s.size(); i++)
+    {
+        cout<<s[i]<<" "<<endl;
+    }
+
+    // sort(s.begin(), s.end()); 
+
+    sort(s.begin(),s.end(),greater<int>());
+    cout<<"after sorting vectore  element  is  : "<<endl;
+    for(int i=0; i<s.size(); i++)
+    {
+        cout<<s[i]<<" "<<endl;
+    }
+    return 0; 
+
+}
+*/ 
+/*
+    int  a[5] = {1,2,2,3,4}
+
+*/
+
+// ex:3 
+/*
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector <int>s ={10,2,2,3,3,4,4,5};
+
+    sort(s.begin(), s.end());  // {2,2,3,3,4,4,5,10}
+
+    s.erase(unique(s.begin(), s.end()),s.end());  // {2,3,4,5,10}
+
+    cout<<"unique element  is  : "<<endl;
+    for(int i=0; i<s.size(); i++)
+    {
+        cout<<s[i]<<" ";
+    }
+    return 0; 
+
+}
+*/ 
+
+// class object vector : 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+class student 
+{
+    public :
+        string  name ; 
+        int  age; 
+    student(string n , int a)
+    {
+        name =n; 
+        age =a;
+    }
+    void  display()
+    {
+        cout<<"name is  : "<<name<<endl;
+        cout<<"age is : "<<age<<endl;
+    }
+};
+int main()
+{
+    vector <student> s;
+
+    s.push_back(student("vivan",20));
+    s.push_back(student("yug",19));
+    s.push_back(student("aastha",17));
+
+    for(auto i :s)
+    {
+        i.display();
+    }
+    return 0;
+}
